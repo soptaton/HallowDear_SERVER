a68dbaeb14a1df077fcf679a2cbc81b6679f0703
hadam <hadam97@naver.co>
2019-11-03 03:41:49 +0900

    pull

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../model/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
